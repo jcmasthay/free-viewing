b4db0640feb57613ef217fc73f7bf5d93c1a7caf
Nick Fagan <nick@fagan.org>
2023-08-04 15:50:38 -0400

    add index for partitioned subset

--- partition_clip_subsets.m
+++ partition_clip_subsets.m
@@ -1,4 +1,4 @@
-function [starts, stops] = partition_clip_subsets(start_ts, stop_ts, clip_len)
+function [starts, stops, store_ii] = partition_clip_subsets(start_ts, stop_ts, clip_len)
 
 total_dur = sum( stop_ts(:) - start_ts(:) );
 num_segs = floor( total_dur / clip_len );
@@ -6,6 +6,7 @@ assert( num_segs > 0, 'Clip is too short: less than %d seconds.', clip_len );
 
 starts = {};
 stops = {};
+store_ii = {};
 
 ii = 1; % interval index
 di = 1; % destination index
@@ -13,6 +14,8 @@ offset = 0;
 leftover = clip_len;
 
 while ( ii <= numel(start_ts) )
+  curr_ii = ii;
+  
   start_t = start_ts(ii) + offset;
   stop_t = min( start_t + leftover, stop_ts(ii) );
   
@@ -29,10 +32,12 @@ while ( ii <= numel(start_ts) )
   if ( di > numel(starts) )
     starts{di, 1} = [];
     stops{di, 1} = [];
+    store_ii{di, 1} = [];
   end
   
   starts{di, 1}(end+1) = start_t;
   stops{di, 1}(end+1) = stop_t;
+  store_ii{di, 1}(end+1) = curr_ii;
   
   if ( leftover == 0 )
     di = di + 1;
@@ -42,5 +47,6 @@ end
 
 starts = starts(1:num_segs);
 stops = stops(1:num_segs);
+store_ii = store_ii(1:num_segs);
 
 end
