8e8b54b6cc2928b6f6bbc8731f50f6cc5be6ac76
jcmasthay <jamie.masthay@yale.edu>
2023-08-04 14:11:56 -0400

    convert minutes to secs

--- run_video_task.m
+++ run_video_task.m
@@ -3,6 +3,13 @@
 clip_table = shared_utils.io.fload( fullfile(project_directory, 'data/clip_table.mat') );
 clip_subset = clip_table(clip_table.VideoFilename == 'Monkey Thieves S2E2', :);
 
+fix_vars = { 'Start', 'Stop' };
+for i = 1:numel(fix_vars)
+  minute = floor( clip_subset.(fix_vars{i}) );
+  sec = clip_subset.(fix_vars{i}) - minute;
+  clip_subset.(fix_vars{i}) = minute * 60 + sec * 100;
+end
+
 %%
 
 win = ptb.Window( [0, 0, 1280, 720] );
@@ -10,7 +17,7 @@ open( win );
 
 err = [];
 try 
-  vid_src_p = fullfile( project_directory, 'videos/clip_0.mp4.avi' );
+  vid_src_p = fullfile( project_directory, 'videos/Monkey Thieves S2E2.avi' );
 
   % start + stop times within respective clips
   [start_ts, stop_ts] = to_clip_subsets( clip_subset.Start, clip_subset.Stop );
@@ -26,3 +33,5 @@ end
 if ( ~isempty(err) )
   rethrow( err );
 end
+
+close( win );
